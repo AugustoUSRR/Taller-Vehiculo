68105ad3313391f9bc2a490d6328b98e2bc5f1fa
Andyy633 <andyy633@gmail.com>
2017-04-28 10:03:24 -0500

    Avance de taller

---
+++ src/taller/de/mècanica/TallerDeMècanica.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller.de.mècanica;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+public class TallerDeMècanica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    Scanner sc= new Scanner(System.in);
+    
+    TallerDeMècanica tallerMecanica = new TallerDeMècanica();
+    
+    
+  
+
+}
